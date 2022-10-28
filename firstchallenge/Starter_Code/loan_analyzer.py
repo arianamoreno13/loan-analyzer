109431651fb3194eb84abcaed3e8855de61f2dc2
Ari <115748516+arianamoreno13@users.noreply.github.com>
2022-10-27 17:41:27 -0700

    Add files via upload
    halfway through the /firstchallenge/Starter_Code/loan_analyzer.py file updates.

--- firstchallenge/Starter_Code/loan_analyzer.py
+++ firstchallenge/Starter_Code/loan_analyzer.py
@@ -81,24 +81,21 @@ print(f"future value of loan {future_value}.")
 #   HINT: Present Value = Future Value / (1 + Discount_Rate/12) ** remaining_months
 discount_rate = 0.20
 present_value= (future_value) / (1 + discount_rate/12) ** remaining_months
-fair_value=(present_value)
-print(f"The fair value of the loan is ${fair_value: .2f}.")
+print(f"The fair value of the loan is ${present_value: .2f}.")
 
-# Its printing fair value and present value as the same value. but I only need one (fairvalue) to print. what is the difference between fair and present?
 
 # If Present Value represents what the loan is really worth, does it make sense to buy the loan at its cost?
 # @TODO: Write a conditional statement (an if-else statement) to decide if the present value represents the loan's fair value.
 #    If the present value of the loan is greater than or equal to the cost, then print a message that says the loan is worth at least the cost to buy it.
 #    Else, the present value of the loan is less than the loan cost, then print a message that says that the loan is too expensive and not worth the price.
-# YOUR CODE HERE!
-present_value = 000
 
-if present_value > fair_value:
+
+if present_value >= loan_costs:
   print(f"the loan is worth at least the cost to buy it.")
-  if present_value > 20:
-    print("and also above 20!")
-  else:
-    print("but not above 20.")
+
+else:
+    present_value < loan_costs
+    print("the loan is too expensive and not worth the price..")
 
 
 """Part 3: Perform Financial Calculations.
@@ -123,13 +120,14 @@ new_loan = {
 # @TODO: Define a new function that will be used to calculate present value.
 #    This function should include parameters for `future_value`, `remaining_months`, and the `annual_discount_rate`
 #    The function should return the `present_value` for the loan.
-# YOUR CODE HERE!
+def new_present_value(future_value, remaining_months, annual_discount_rate)
+    new_present_value = future_value / (1+ (annual_discount_rate / 12)) ** remaining_months
 
 
 # @TODO: Use the function to calculate the present value of the new loan given below.
 #    Use an `annual_discount_rate` of 0.2 for this new loan calculation.
-# YOUR CODE HERE!
-print(f"The present value of the loan is: {present_value}")
+annual_discount_rate = 0.2
+print(f"The present value of the loan is: {new_present_value}")
 
 
 """Part 4: Conditionally filter lists of loans.
@@ -171,7 +169,12 @@ loans = [
 ]
 
 # @TODO: Create an empty list called `inexpensive_loans`
-# YOUR CODE HERE!
+inexpensive_loans =
+        annual_discount_rate = 0.20
+        present_value = calculate_present_value(
+        new_loan["future_value"],
+        new_loan["remaining_months"],
+        annual_discount_rate)
 
 # @TODO: Loop through all the loans and append any that cost $500 or less to the `inexpensive_loans` list
 # YOUR CODE HERE!
